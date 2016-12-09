f2689095136acf1c0de8d3a0bce38a5c80b84489
vekotinkero <veikko.kero@gmail.com>
2016-12-09 13:36:42 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -39,6 +39,7 @@ Type=Application
 Name=Reboot to HDMI
 Exec=/home/pi/LCD_show_v6_1_3/LCD_hdmi
 Icon=/usr/share/icons/Moka/32x32/devices/computer.png
+Terminal=true
 EOF
 
 sudo cat > LCD.desktop << EOF
@@ -47,6 +48,7 @@ Type=Application
 Name=Reboot to LCD
 Exec=/home/pi/LCD_show_v6_1_3/LCD35_v
 Icon=/usr/share/icons/Moka/32x32/devices/input-tablet.png
+Terminal=true
 EOF
 
 # Make them all executable
@@ -55,8 +57,8 @@ sudo chmod +x LCD.desktop HDMI.desktop keyboard.desktop
 # We need to fix the Osoyoo Scripts a bit, now the only work locally
 # We will add path to the beginning 
 cd /home/pi/LCD_show_v6_1_3
-echo 'cd /home/pi/LCD_show_v6_1_3' | cat - ./LCD35_v > temp && mv temp ./LCD35_v
-echo 'cd /home/pi/LCD_show_v6_1_3' | cat - ./LCD_hdmi > temp && mv temp ./LCD_hdmi
+echo $'echo Rebooting to LCD!\ncd /home/pi/LCD_show_v6_1_3' | cat - ./LCD35_v > temp && mv temp ./LCD35_v
+echo $'echo Rebooting to HDMI!\ncd /home/pi/LCD_show_v6_1_3' | cat - ./LCD_hdmi > temp && mv temp ./LCD_hdmi
 
 echo "All done, baby! Let's reboot and start the LCD"
 sudo /home/pi/LCD_show_v6_1_3/LCD35_v
